3e7562941b142ea04766e478491c47be1a6972cb
bernat <bernat.costa@cesurformacion.com>
2022-02-12 23:51:22 +0100

    cargarMaterialGrande

---
+++ Ejercicios/4.DML.Consultas/5.Soccer.sql
@@ -0,0 +1,40 @@
+USE SOCCER
+
+
+--1.Competiciones de España
+
+
+--2.Equipos de la liga española
+
+
+--3.Equipo con mas goles en casa
+
+
+--4.equipo con mas goles en casa y fuera
+
+
+--5.equipo con con mas victorias
+
+
+--6.Jugadores del betis/sevilla la temporada 2015/2016
+
+
+--7.top 20 de los equipos que mas empatan.
+
+
+--8.equipos con los jugadores con la media de altura mas altos.
+
+
+--9.Equipo que mas veces ha perdido un partido.
+
+
+--10.jugador mas joven.
+
+
+--11.¿Hay algun jugador que cumplaaños hoy, siendo hoy, la fecha en la que se lance la consulta?
+
+
+--12.que dia del año hay mas cumpleaños?
+
+
+--13.¿Que mejoras podriamos hacer en el diseño de la bbdd para facilitar las consultas?
