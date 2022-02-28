c4d67981b88d26b3d5fba42f7cbb053fbc04c6ac
bernat <bernat.costa@cesurformacion.com>
2022-02-28 21:53:44 +0100

    Ejercicios modificaciones

---
+++ Ejercicios/5.DML.Modificaciones/1.Arepazo.sql
@@ -0,0 +1,36 @@
+--1.-Da de alta un cliente en el arepazo
+
+
+--2.-Da de alta un nuevo plato en el arepa<o. Patatas bravas que cueste 3e.
+
+
+--3.-Ese plato, lleva 0.2kg de patatas. Añade tb el ingrediente y la receta.
+
+
+--4.-Añade un pedido de 3 raciones de patatas bravas, 3 cervezas a ese nuevo cliente.
+
+
+--5 añade y borra un cliente.
+
+--6.- Borra todos los clientes que no tengan pedidos realizados.
+
+--7.- Borra los ingredientes que no se usan en ninguna receta.
+
+
+--8.- Cambia la dirección del cliente 2, la nueva dirección es Calle Falsa 44
+
+--9.- Modifica la descripción de las patatas bravas añadidas en el ejercicio 2.
+
+--10. Microsoft ha decidido cerrar hotmail. Ahora todos los correos de hotmail seran outlook. 
+    --Actualiza la bbdd de clientes.
+
+--11. Incrementa el precio de todos los elementos del menú un 5%.
+
+--12 Los pedidos del 2022 tb deben verse afectados por ese incremento del 5%
+            --Recalcula el subtotal, y el total de todos los pedidos del 2022.
+
+--13.- Borra todos los elementos del menú que nadie ha pedido nunca.
+
+--14.- Añade un pedido a fecha de hoy para el cliente 5. Añadele 5 unidades del menu 1. 
+--Recalcula todos los precios con el mínimo número de instrucciones SQL y intentando no poner nada "a pelo"
+-- que no sea estrictamente necesario.
---
+++ Ejercicios/5.DML.Modificaciones/2.-Futbol.sql
@@ -0,0 +1,37 @@
+--1.-Añadete como jugador
+
+--2,. añade a 10 jugadores más.
+
+--3.- Crea un equipo con el nombre que quieras.
+
+--4.- sumalo a laliga española.
+
+--5.-Añade 5 partidos contra los equipos que tu quieras, jugados en 2022, Inventate los resultados.
+
+
+--6.- Inventare las estadisticas de los jugadores ( goles, minutos jugados, asistentencias,tarjetas...
+
+
+--7 asegurate que en la tabla PartidosJugadores, no se añaden mas goles que lo que dice el marcador en la tabla Partidos.
+
+
+--8.- Si has encontrado algun error, actualiza el resultado en la tabla partidos. Podrias hacer un update que actualizara 
+---el marcador de TODOS LOS PARTIDOS mirando los goles de PARTIDOSJUGADORES?
+
+
+--9.-Ficha al jugador que quieras para tu equipo. Cambialo de equipo y ponlo a jugar.
+
+--10.- añadete una tajeta amarilla a ti en tu último partido.
+
+
+--11.- Borra aquellos jugadores que no hayan jugado ningun partido.
+
+--12.- laliga decide expulsar al Real Madrid y Barcelona por tener el presupuesto hinchado.
+        -- Eliminalos de la bbdd. Borra todo lo estrictamente necesario.
+
+
+ --13.-Al finalizar el segundo partido de tu equipo, cambias el entrenador. Actualiza el nombre del entrenador
+
+
+ --14 Que cambio harias en la bbdd para poder guardar el historico de entrenadores de cada equipo? 
+   -- haz los cambios en el DDL y luego actualiza la información de ese cambio de entrenador de tu equipo.
