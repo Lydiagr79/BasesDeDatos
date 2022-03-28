39dcafc2555bb0b27c81ffcd956b0a88b9a7d3d9
bernat <bernat.costa@cesurformacion.com>
2022-03-28 20:57:50 +0200

    procedures ejercicios

---
+++ Ejercicios/6.T-SQL/3.Procedures.sql
@@ -0,0 +1,52 @@
+--1-Crear un procedimiento almacenado que nos devuelva el factorial de un número​
+
+--2-Crear un procedimiento almacenado que nos indique si un numero es primo o no.​
+
+--3-Crear un procedimiento almacenado que nos devuelva los n primeros numeros de la sucesión de fibonacci.​
+
+--4-Con adventureWorks, dame la lista de empleados de un departamento indicando el id del departamento.​
+
+--5-A la consulta anterior, añadir un parametro opcional que sea el sexo de los empleados​
+
+--6 añade al procedimiento anterior un parametro de salida que nos devuelva el numero de empleados.
+
+
+
+--Manipulando cadenas...
+
+--6-Dime que clientes del arepazo, tienen un nombre de mas de 5 caracteres.​
+
+--7-Recorta a 5 caracteres los nombres de los clientes del arepazo. Muestralo por pantalla, no lo guardes en ningun sitio.​
+
+--8-De los email del arepazo, obten los caracteres del 3 al 7​
+
+--9-Dime el total de caracteres de todos los nombres de los clientes del arepazo.​
+
+--10-Haz un procedimiento, dado un cliente del arepazo( el id), me devuelva una cadena de texto con su nombre, el total gastado, y el numero de pedidos. Ejemplo:​
+
+--'El cliente Bernat, ha gastado 100€ en un total de 4 pedidos.'​
+
+
+
+
+
+
+
+
+
+
+
+create procedure holamundo
+AS
+BEGIN
+print 'hola mundo'
+END
+
+exec holamundo
+
+
+print len('dfgdfhdgndfghfdg fdgh dfg')
+
+
+
+
