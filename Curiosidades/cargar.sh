05ee2e94e0b0570e5871bdf93165300912a531aa
bernat <bernat.costa@cesurformacion.com>
2022-04-18 13:23:28 +0200

    script cargar bbdd sin lineas en blanco

--- Curiosidades/cargar.sh
+++ Curiosidades/cargar.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 directorio=$2
-
 for i in $(ls $directorio)
 do
-   
-   
  docker exec -it $1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 12345Ab## -Q "RESTORE DATABASE ${i%%.*}  FROM DISK = N'/var/opt/mssql/$2/$i'"
 done
