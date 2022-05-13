929db3ef7c1eb0952b20cf6ce18033f8f094f381
bernat <bernat.costa@cesurformacion.com>
2022-05-13 07:41:36 +0200

    procedures nba

---
+++ Ejercicios/6.T-SQL/5.funcionesytriggersNBA.sql
@@ -0,0 +1,36 @@
+use nba
+
+--1. guarda el nombre del jugador mas joven de la nba en una variabla
+
+--2.- imprime por pantalla su nombre
+
+--3.- imprime por pantalla su nombre en vertical. Por ejemplo, si es Ricky Rubio, que aparezca
+/*
+R
+i
+c
+y
+
+R
+u
+b
+i
+o
+
+*/
+
+
+--3 dime la posición en la que el nombre del jugador tiene un espacio en blanco
+
+--4- dime cuantas letras a tiene ese nombre.
+
+
+--5 Dime en que posiciones está la letra a en ese nombre.
+
+
+--6 dime si tiene la letra i
+
+
+--7 pon en otra variable todas las letras del alfabeto
+
+--8 pinta el nombre del jugador cambiando cada letra por la siguiente en el alfabeto.
