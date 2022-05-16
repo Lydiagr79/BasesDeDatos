d753ed562ccd897ff1ee253b14309812c4b39f3c
bernat <bernat.costa@cesurformacion.com>
2022-05-16 07:10:12 +0200

    cursores y mas triggers

--- Ejercicios/7-Triggers/1.triggers.sql
+++ Ejercicios/7-Triggers/1.triggers.sql
@@ -9,3 +9,10 @@ use books
 --4 haz otro trigger para que cada vez que se cambie o inserte el numero de paginas de un libro, se recalcule el campo peso según la lógica siguiente:
 -- peso cartulina ( portada + contraportada) : 0.150 kg
 -- peso folio ( 1 pagina = 2 folios) : 0.0016 kg
+
+--5 haz un trigger para que cada vez que se añada un libro a un pedido, recalcule los gastos de envio teniendo en cuenta el peso de ese libro.
+
+--6.- haz un trigger para que cada vez que se añada un libro a un pedido, se recalcule el subtotal.
+
+--7. En el Arepezo, tenemos una tabla Correos. Haz un trigger para que cada vez que se modifique el codPostal, se modifique el campo provincia. Los dos primeros digitos del 
+--cod postal indican la provincia. En la tabla Correos, tenemos los dos primeros digitos de cada provincia.
