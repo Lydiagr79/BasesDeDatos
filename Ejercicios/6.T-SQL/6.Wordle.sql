d8dd4b359a33ce446044b17e0aba71ea4be01229
bernat <bernat.costa@cesurformacion.com>
2022-05-20 08:44:25 +0200

    ejercicios wordle

---
+++ Ejercicios/6.T-SQL/6.Wordle.sql
@@ -0,0 +1,18 @@
+
+--1.-crea un método que nos devuelva 0 o 1 dependiendo de si el jugador existe en la bbdd 
+-- o no. Le pasaremos el nombre del jugador
+
+--2.-crea un método para insertar un jugador a la bbdd del wordle. 
+--Al método le pasaremos el nombre del jugador
+
+--3.-crea un método que, pasandole un nombre de un jugador, 
+--compruebe si existe en la bbdd y si no existe, lo dé de alta (lo inserte)
+
+--4.-Crea un método para que me devuelva la palabra del dia
+
+--5,-Crea un método para que, dado un jugador,
+-- me diga cuantos intentos lleva en el dia de hoy.
+
+--6.-Crea un método que me diga la posición de una letra en un varchar. 
+--Le pasaremos un varchar y una letra y nos devolverá la posición. 
+--Nos devolverá 0 o -1 si la letra no está en el varchar.
